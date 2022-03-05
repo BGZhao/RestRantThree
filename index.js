38113f7134ab08473bdfeaa7e8c7da94b8fb0225
BGZhao <92346786+BGZhao@users.noreply.github.com>
2022-03-05 11:21:32 -0600

    Third part of RestRant

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('*', (req, res) => {
+  res.render('error404')
+})
+// GET /places
+app.get('/', (req, res) => {
+  let places = []
+  res.render('places/index')
+})
+app.listen(process.env.PORT,function(){
+  console.log('running',process.env.PORT)
+})
